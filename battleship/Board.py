44f6a3e3801de243aa7763a87b5032acf7c80aa2
junhui99 <50338971+junhui99@users.noreply.github.com>
2020-01-25 17:45:40 -0800

    Add files via upload

--- battleship/Board.py
+++ battleship/Board.py
@@ -1,6 +1,8 @@
-r  = 8 #row
-c = 5 #col
-board = [['*' for column in range(r)] for row in range(c)]
-print(' ' + ' '.join(map(str, range(r)))) # print column labels
-for row, item in enumerate(board): # for each row
-    print(str(r)  + ' '.join(item))
+import sys
+r  = sys.argv[0] #number of row
+c = sys.argv[1] #number of column
+board = [['*' for row in range (r)] for col in range (c)]
+print('  ' + ' '.join(map(str, (range(r)))))
+# print the column labels along with the corresponding label
+for col, sign in enumerate(board): # for each column, we first print the number of the column
+    print(str(col) + ' ' + ' '.join(sign)) #then we print *
